cc083c07266196b67d4eeb24df594a4c48b54e2b
daniel <g.thirteen333@gmail.com>
2019-07-30 17:41:02 -0400

    todo form added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,37 @@ import React, {useState} from 'react';
 
 import './App.css';
 
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+
 function App() {
   const [todos, setTodos] = useState([{text: "Learn about React"}, {text: "Meet friend for lunch"}, {text: "Build really cool todo app"}]);
    const Todo = ({todo}) => <div className="todo">{todo.text}</div>
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  }
+
   return (
     <div className="App">
       <h1>TODO REACT HOOK APP</h1>
@@ -13,6 +41,7 @@ function App() {
       {todos.map((todo, index) => (
         <Todo key={index} index={index} todo={todo}/>
       ))}
+      <TodoForm addTodo={addTodo} />
      </div>
     </div>
   );
